af0674970ebbad01345efd859da7e7f7fb8b20f1
Kamil Maka <kamil.maka.trk@gmail.com>
2016-06-02 01:26:01 +0100

    Add files via upload

---
+++ spellbuyer.lua
@@ -0,0 +1,460 @@
+local vocation = "knight" --- set your vocation (knight, paladin, sorcerer, druid)
+
+--- I don't set it to buy spells to choose between like ultimate xxx strike, or swift foot/sharpshooter etc
+--- but I set it to buy rage of the skies and eternal winter
+
+
+Self.SayToNpc("Hi")
+if(vocation == "knight") then
+	if(Self.Level()>=8) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Find Person")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Wound Cleansing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=10) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Magic Rope")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Cure Poison")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=16) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Levitate")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Haste")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Brutal Strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=20)then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Challange")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=28) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Charge")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Whirlwind Throw")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=35) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("train party")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("groundshaker")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("berserk")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=50) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("inflict wound")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("recovery")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=70) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("front sweep")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=80) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("intense wound cleansing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=90) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("fierce berserk")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=100) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("intense recovery")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=110) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("annihilation")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		end
+		end
+		end
+		end
+		end
+		end
+		end
+		end
+		end
+		end
+		end
+		end
+elseif(vocation == "paladin") then
+	if(Self.Level()>=8) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("light healing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("find person")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=10) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("magic rope")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("cure poison")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=14) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("levitate")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("haste")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=20) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("intense healing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=23) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("ethereal spear")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=35) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("protect party")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("divine healing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=40) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("divine missile")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=50) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("enchant spear")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("divine caldera")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("recovery")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=60) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("salvation")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=90) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Cure Curse")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Strong Ethereal Spear")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=100) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("intense recovery")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+elseif(vocation == "sorcerer") then
+	if(Self.Level()>=8) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("light healing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("find person")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=10) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("magic rope")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("cure poison")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=15) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("energy strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("levitate")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("terra strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("flame strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("magic shield")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("haste")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("ice strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=20) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("death strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("fire wave")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("strong haste")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("intense healing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=25) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("energy beam")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("summon creature")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=30) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("great energy beam")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("ultimate healing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=40)then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("enchant party")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("invisible")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("energy wave")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=55) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("lightning")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("rage of the skies")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=80) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("strong flame strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("strong energy strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+elseif(vocation == "druid") then
+	if(Self.Level()>=8) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("find person")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("light healing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=10) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("magic rope")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("cure poison")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=15) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("energy strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("levitate")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("terra strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("food")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("flame strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("magic shield")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("haste")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("ice strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=20) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("physical strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("heal friend")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("ice wave")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("intense healing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("strong haste")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=25) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Summon Creature")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=30) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Ultimate Healing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=40) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("heal party")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("invisible")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("mass healing")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("terra wave")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("strong ice wave")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=60) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("eternal winter")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=70) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("strong terra strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	if(Self.Level()>=80) then
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("strong ice strike")
+		sleep(math.random(200, 1000))
+		Self.SayToNpc("Yes")
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+end
